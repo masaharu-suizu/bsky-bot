29330e146978e082eae5cc9bb32f7451db7e2529
Masaharu Suizu <masaharu.sui@gmail.com>
2025-11-30 15:14:27 +0900

    Update main.py and yml for GitHub action

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from bsky-bot!")
+import os
+from atproto import Client
 
+def main():
+    handle = os.environ["BSKY_HANDLE"]
+    app_password = os.environ["BSKY_APP_PASSWORD"]
+
+    client = Client()
+    client.login(handle, app_password)
+    client.send_post("(これはPython SDKを使ったbotです。)\nHello world!")
 
 if __name__ == "__main__":
     main()
