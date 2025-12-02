6a21ee5a9b4bfae6106ac22586f3941a4e7a722b
Masaharu Suizu <masaharu.sui@gmail.com>
2025-12-02 19:14:01 +0900

    ボットのメッセージの絵文字を曜日ごとに出し分けるロジックを追加

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-import os
 from atproto import Client
+import datetime
+import os
+
+def emoji():
+    day_of_week = datetime.date.today().weekday()
+
+    if day_of_week == 0:    # Monday
+        return "🍛"
+    elif day_of_week == 1:  # Tuesday
+        return "🍜"
+    elif day_of_week == 2:  # Wednesday
+        return "🍣"
+    elif day_of_week == 3:  # Thursday
+        return "🍔"
+    elif day_of_week == 4:  # Friday
+        return "🍕"
+    elif day_of_week == 5:  # Saturday
+        return "🌭"
+    elif day_of_week == 6:  # Sunday
+        return "🍱"
+
 
 def main():
     handle = os.environ["BSKY_HANDLE"]
@@ -7,7 +27,7 @@ def main():
 
     client = Client()
     client.login(handle, app_password)
-    client.send_post("今日のランチは何を食おうかなー🍙")
+    client.send_post("(Botです) 今日のランチは何を食おうかなー" + emoji())
 
 if __name__ == "__main__":
     main()
